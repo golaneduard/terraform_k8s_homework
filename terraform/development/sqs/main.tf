44cee85f7e97e3fd6e90c352cbf8bf6852056296
Eduard Golan <eduard.golan@softwaremind.com>
2025-04-07 23:16:57 +0300

    Remove redrive_policy for development env

--- terraform/development/sqs/main.tf
+++ terraform/development/sqs/main.tf
@@ -1,18 +1,7 @@
-resource "aws_sqs_queue" "queue_deadletter" {
-  name = "${var.env}-terraform-example-deadletter-queue"
-
-  tags = merge(var.root_tags, var.env_tags)
-}
-
 resource "aws_sqs_queue" "queue" {
   name = "${var.env}-terraform-example-queue"
   max_message_size          = 2048
   message_retention_seconds = 262144
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.queue_deadletter.arn
-    maxReceiveCount     = 4
-  })
 
   tags = merge(var.root_tags, var.env_tags)
 }
